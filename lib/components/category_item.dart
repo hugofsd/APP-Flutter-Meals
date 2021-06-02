a1e065f3971a74ce1e4553c616a174392397d852
hugofsd <hugo619@outlook.com>
2021-06-02 14:03:17 -0300

    Passando dados via constructor

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -15,7 +15,7 @@ class CategoryItem extends StatelessWidget {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (_) {
-          return CategoriesMealsScreen();
+          return CategoriesMealsScreen(category);
         },
       ),
     );
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+  final Category category;
+  const CategoriesMealsScreen(this.category);
   @override
   Widget build(BuildContext context) {
     //Scaffold: andaime
     return Scaffold(
       appBar: AppBar(
-        title: Text('Receita'),
+        title: Text(category.title),
       ),
-      body: Center(child: Text('Receitar por categoria')),
+      body: Center(child: Text('Receitar por categoria ${category.id}')),
     );
   }
 }
