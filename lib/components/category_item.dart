aa70e3666e35d9f47a48ea6f1935b0de6c8ec6fb
hugofsd <hugo619@outlook.com>
2021-06-02 11:11:24 -0300

    Navigator

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -2,32 +2,51 @@ import 'package:flutter/material.dart';
 
 import '../models/category.dart';
 
+import '../screens/categories_meals_screen.dart';
+
 class CategoryItem extends StatelessWidget {
   //referenciando o model para a variavel depois do import
   final Category category;
   //item recebe categoria
   const CategoryItem(this.category);
 
+//navigator
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoriesMealsScreen();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.title,
-      ),
-      padding: EdgeInsets.all(15), //centralizar o text. "preenchimento"
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15), // borda circular
-          gradient: LinearGradient(
-              colors: [
-                category.color.withOpacity(0.3),
-                category.color,
-              ],
-              //alinhamento
-              begin: Alignment.topLeft, //começo da grid
-              end: Alignment.bottomRight // canto inferior direito
+    //InkWell para saber se o usuário cliclou
+    return InkWell(
+      onTap: () => _selectCategory(context), // quando o user clica
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Colors.black,
+      child: Container(
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        padding: EdgeInsets.all(15), //centralizar o text. "preenchimento"
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15), // borda circular
+            gradient: LinearGradient(
+                colors: [
+                  category.color.withOpacity(0.3),
+                  category.color,
+                ],
+                //alinhamento
+                begin: Alignment.topLeft, //começo da grid
+                end: Alignment.bottomRight // canto inferior direito
 
-              )),
-    );
+                )),
+      ),
+    ); //pois o contairner n recebe OnPrees de click
   }
 }
