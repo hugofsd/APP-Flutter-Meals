bd8c4e80ab9d3f8981214d298becfcfc10001f2a
hugofsd <hugo619@outlook.com>
2021-06-02 16:14:53 -0300

    Selecionando ref por categoria

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import '../models/category.dart';
 
+//import dos dados
+import '../data/dummy_data.dart';
+
 class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //convertendo o argumento para categoria, rota
     final category = ModalRoute.of(context).settings.arguments as Category;
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     //Scaffold: andaime
     return Scaffold(
-      appBar: AppBar(
-        title: Text(category.title),
-      ),
-      body: Center(child: Text('Receitar por categoria ${category.id}')),
-    );
+        appBar: AppBar(
+          title: Text(category.title),
+        ),
+        body: ListView.builder(
+            itemCount: categoryMeals.length, //quantidade de elementos
+            itemBuilder: (ctx, index) {
+              return Text(categoryMeals[index].title);
+            }));
   }
 }
