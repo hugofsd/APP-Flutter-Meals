2b6cdedf37aa09fed6ec3c5b21f2e6be0c1567fa
hugofsd <hugo619@outlook.com>
2021-06-03 17:11:27 -0300

    Reaproveitando componentes, habilitando SingleChildScrollView para corrigir rolagem

--- lib/screens/meal_detail_screens.dart
+++ lib/screens/meal_detail_screens.dart
@@ -3,6 +3,32 @@ import 'package:flutter/material.dart';
 import '../models/meal.dart';
 
 class MealDetailScreen extends StatelessWidget {
+// componente para reaproveitar titulo
+  Widget _createSelectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.title,
+      ),
+    );
+  }
+
+// //contairner
+//   Widget _createSectionContairner(Widget child) {
+//     Container(
+//       width: 300,
+//       height: 210,
+//       padding: EdgeInsets.all(10),
+//       margin: EdgeInsets.all(10),
+//       decoration: BoxDecoration(
+//           color: Colors.white,
+//           border: Border.all(color: Colors.grey),
+//           borderRadius: BorderRadius.circular(10)),
+//       child: child,
+//     );
+//   }
+
   @override
   Widget build(BuildContext context) {
     //passando os dados de Meal para a var
@@ -12,50 +38,70 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 200,
-            width: double.infinity,
-            child: Image.network(
-              meal.imageUrl,
-              fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 200,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            child: Text(
-              'Ingredientes',
-              style: Theme.of(context).textTheme.title,
-            ),
-          ),
-          Container(
-            width: 300,
-            height: 210,
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10)),
-            child: ListView.builder(
-              itemCount: meal.ingredients
-                  .length, // percorrer apenas a quantidade de itens da lista
-              itemBuilder: (ctx, index) {
-                return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 5,
-                      horizontal: 10,
+            _createSelectionTitle(context, 'Ingredientes'),
+            Container(
+              width: 300,
+              height: 210,
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10)),
+              child: ListView.builder(
+                itemCount: meal.ingredients
+                    .length, // percorrer apenas a quantidade de itens da lista
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10,
+                      ),
+                      child: Text(meal.ingredients[index]),
                     ),
-                    child: Text(meal.ingredients[index]),
-                  ),
-                  color: Theme.of(context).accentColor,
-                );
-              },
+                    color: Theme.of(context).accentColor,
+                  );
+                },
+              ),
             ),
-          )
-        ],
+            _createSelectionTitle(context, 'Passos'),
+            Container(
+              width: 300,
+              height: 210,
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10)),
+              child: ListView.builder(
+                itemCount: meal.steps
+                    .length, // percorrer apenas a quantidade de itens da lista
+                itemBuilder: (ctx, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      child:
+                          Text('${index + 1}'), //+1 para começar a partir do 1
+                    ),
+                    title: Text(meal.steps[index]),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
