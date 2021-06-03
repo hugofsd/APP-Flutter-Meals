c6c3fe7b6779f5de636f5218757edffe12dffe11
hugofsd <hugo619@outlook.com>
2021-06-03 18:46:23 -0300

    Tab na parte inferior

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -3,33 +3,43 @@ import 'package:flutter/material.dart';
 import '../screens/categories_screens.dart';
 import '../screens/favorite_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+
+  final List<Widget> _screens = [
+    CategoriesScreens(),
+    FavoriteScreen(),
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2, //para definir a quantidade de abas
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Refeições'),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categorias',
-              ),
-              Tab(
-                icon: Icon(Icons.favorite),
-                text: 'Favoritos',
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            CategoriesScreens(),
-            FavoriteScreen(),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Refeições'),
+      ),
+      body: _screens[_selectedScreenIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedScreenIndex, //reconhecer tab selecionada
+        // backgroundColor: Theme.of(context).primaryColor,
+        onTap: _selectScreen,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category), title: Text('Categorias')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.star), title: Text('Favoritos')),
+        ],
       ),
     );
   }
