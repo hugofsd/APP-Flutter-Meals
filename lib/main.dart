8746626becb13211a9384bc912574a6fd2302bb2
hugofsd <hugo619@outlook.com>
2021-06-03 21:07:58 -0300

    Filtrar dados 01

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,20 @@ import 'utils/app_routes.dart';
 import 'screens/tabs_screen.dart';
 import 'screens/settings_screens.dart';
 
+import 'models/meal.dart';
+import 'data/dummy_data.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // receber comidas disponiveis, recebe todas de primeira por padrão
+  List<Meal> _availableMeals = DUMMY_MEALS;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,7 +42,8 @@ class MyApp extends StatelessWidget {
       // initialRoute: , para rota inical é outra auternativa
       routes: {
         AppRoutes.HOME: (ctx) => TabsScreen(), //rota principal
-        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) =>
+            CategoriesMealsScreen(_availableMeals),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
         AppRoutes.SETTINGS: (ctx) => SettingsScreen()
       },
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -2,18 +2,25 @@ import 'package:flutter/material.dart';
 import '../models/category.dart';
 
 //import dos dados
-import '../data/dummy_data.dart';
+// import '../data/dummy_data.dart';
 
 //importando o componentes Ref
 import '../components/meal_item.dart';
 
+import '../models/meal.dart';
+
 class CategoriesMealsScreen extends StatelessWidget {
+  // para receber comida por parametro
+  final List<Meal> meals;
+
+  const CategoriesMealsScreen(this.meals);
+
   @override
   Widget build(BuildContext context) {
     //convertendo o argumento para categoria, rota
     final category = ModalRoute.of(context).settings.arguments as Category;
 
-    final categoryMeals = DUMMY_MEALS.where((meal) {
+    final categoryMeals = meals.where((meal) {
       return meal.categories.contains(category.id);
     }).toList();
 
